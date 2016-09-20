c7c1c4650ca8af9da37696951497fbb9c16c2590
Andrews Medina <andrewsmedina@gmail.com>
2016-09-20 14:31:24 -0300

    using simple=true parameter to cloudstack api

--- main.go
+++ main.go
@@ -1,31 +1,77 @@
 package main
 
 import (
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/base64"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"net/url"
+	"sort"
+	"strings"
 
-	"github.com/atsaki/golang-cloudstack-library"
+	"github.com/tsuru/tsuru/net"
 )
 
-func listMachines(client *cloudstack.Client) ([]*cloudstack.VirtualMachine, error) {
-	projectsParams := cloudstack.NewListProjectsParameter()
-	projects, err := client.ListProjects(projectsParams)
+type client struct {
+	apiKey    string
+	secretKey string
+	url       string
+}
+
+type ListVirtualMachinesResponse struct {
+	ListVirtualMachinesResponse struct {
+		VirtualMachine []VirtualMachine `json:"virtualmachine"`
+	} `json:"listvirtualmachinesresponse"`
+}
+
+type VirtualMachine struct {
+	Nic []NicStruct `json:"nic"`
+}
+
+type NicStruct struct {
+	IpAddress string `json:"ipaddress"`
+}
+
+type ListProjectsResponse struct {
+	ListProjectsResponse struct {
+		Project []Project `json:"project"`
+	} `json:"listprojectsresponse"`
+}
+
+type Project struct {
+	Id string
+}
+
+func listMachines(c *client) ([]string, error) {
+	params := map[string]string{"simple": "true"}
+	var projects ListProjectsResponse
+	err := c.do("listProjects", params, &projects)
 	if err != nil {
 		return nil, err
 	}
-	var machines []*cloudstack.VirtualMachine
-	for _, p := range projects {
-		machinesParams := cloudstack.NewListVirtualMachinesParameter()
-		machinesParams.ProjectId = p.Id
-		m, err := client.ListVirtualMachines(machinesParams)
-		if err == nil {
-			machines = append(machines, m...)
+	var machines []string
+	for _, p := range projects.ListProjectsResponse.Project {
+		params := map[string]string{
+			"projectid": p.Id,
+			"simple":    "true",
+		}
+		var m ListVirtualMachinesResponse
+		err = c.do("listVirtualMachines", params, &m)
+		if err != nil {
+			return nil, err
+		}
+		for _, vm := range m.ListVirtualMachinesResponse.VirtualMachine {
+			for _, n := range vm.Nic {
+				machines = append(machines, n.IpAddress)
+			}
 		}
 	}
-	return machines, nil
+	return machines, err
 }
 
 func main() {
@@ -36,23 +82,63 @@ func main() {
 		secretKey = flag.String("secret-key", "", "cloudstack secret key")
 	)
 	flag.Parse()
-	endpoint, err := url.Parse(*address)
-	if err != nil {
-		log.Fatal("Error parsing url:", err)
+	c := &client{
+		apiKey:    url.QueryEscape(*apiKey),
+		secretKey: url.QueryEscape(*secretKey),
+		url:       *address,
 	}
-	client, err := cloudstack.NewClient(
-		endpoint,
-		url.QueryEscape(*apiKey),
-		url.QueryEscape(*secretKey),
-		"",
-		"",
-	)
-	if err != nil {
-		log.Fatal("Error creating client: ", err)
-	}
-	machines, err := listMachines(client)
+	fmt.Println(c)
+	machines, err := listMachines(c)
 	if err != nil {
 		log.Fatal("Error list machines: ", err)
 	}
 	fmt.Println("machines: ", machines)
+}
+
+func (c *client) buildUrl(command string, params map[string]string) (string, error) {
+	params["command"] = command
+	params["response"] = "json"
+	params["apiKey"] = c.apiKey
+	var sorted_keys []string
+	for k := range params {
+		sorted_keys = append(sorted_keys, k)
+	}
+	sort.Strings(sorted_keys)
+	var string_params []string
+	for _, key := range sorted_keys {
+		queryStringParam := fmt.Sprintf("%s=%s", key, url.QueryEscape(params[key]))
+		string_params = append(string_params, queryStringParam)
+	}
+	queryString := strings.Join(string_params, "&")
+	digest := hmac.New(sha1.New, []byte(c.secretKey))
+	digest.Write([]byte(strings.ToLower(queryString)))
+	signature := base64.StdEncoding.EncodeToString(digest.Sum(nil))
+	return fmt.Sprintf("%s?%s&signature=%s", c.url, queryString, url.QueryEscape(signature)), nil
+}
+
+func (c *client) do(cmd string, params map[string]string, result interface{}) error {
+	u, err := c.buildUrl(cmd, params)
+	if err != nil {
+		return err
+	}
+	client := net.Dial5Full300ClientNoKeepAlive
+	resp, err := client.Get(u)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("Unexpected response code for %s command %d: %s", cmd, resp.StatusCode, string(body))
+	}
+	if result != nil {
+		err = json.Unmarshal(body, result)
+		if err != nil {
+			return fmt.Errorf("Unexpected result data for %s command: %s - Body: %s", cmd, err.Error(), string(body))
+		}
+	}
+	return nil
 }
